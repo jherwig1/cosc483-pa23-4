163790ef4684624342492b622f0a631001da6ea5
Tyler Stuessi <tstuessi@eecs.utk.edu>
2016-11-13 22:28:00 -0500

    FIX IO STUFF

--- rsa_decrypt.cpp
+++ rsa_decrypt.cpp
@@ -5,6 +5,8 @@
  */
 
 #include <fstream>
+#include <iostream>
+#include <string.h>
 #include <openssl/bn.h>
 
 #include "rsa_decrypt.h"
@@ -20,7 +22,7 @@ void rsa_decrypt(string &keyfile, string &inputfile, string &outputfile) {
 	string private_key, N, encrypted, plaintext;
 	char *temp_string;
 	int security_param, i;
-	UCHAR padded;
+	//UCHAR padded;
 
 	BIGNUM *BN_vals[3];
 	BIGNUM *BN_N;
@@ -57,18 +59,40 @@ void rsa_decrypt(string &keyfile, string &inputfile, string &outputfile) {
 	fin >> encrypted;
 	fin.close();
 
+	cout << encrypted << endl;
+
+	int padded = 0;
+
 	//for each block of input, decrypt it and delete the padding
 	//then add it to the output
-	for(i = 0; i < encrypted.size(); i += security_param) {
-		BN_hex2bn(BN_vals + 2, encrypted.c_str() + i);
+	for(i = 0; i < encrypted.size(); i += security_param / 4) {
+		char *block = (char *)malloc((security_param / 4) + 1);
+		strncpy(block, encrypted.c_str() + i, security_param / 4);
+		block[(security_param / 4)] = 0x00;
+		BN_hex2bn(BN_vals + 2, block);
 		BN_encrypted = BN_vals[2];
 
 		BN_mod_exp(BN_plaintext, BN_encrypted, BN_private_key, BN_N, ctx);
+		//BN_copy(BN_plaintext, BN_encrypted);
+
+		//create masking stuff
+		BIGNUM *BN_padding = BN_new();
+
+		BN_copy(BN_padding, BN_plaintext);
+		BN_mask_bits(BN_padding, 8);
+		padded = BN_get_word(BN_padding);
 
 		//take off all the RSA padding
 		temp_string = BN_bn2hex(BN_plaintext);
-		plaintext += temp_string + (security_param / (8*2)) + 3;
+		BN_print_fp(stdout, BN_plaintext);
+		cout << endl;
+		cout << temp_string << endl;
+		//only increment by the randomness and 2 of the 3 added bytes
+		//it cuts off the leading zeros
+		plaintext += temp_string + 2*(security_param / (8*2)) + 2*2;
 		OPENSSL_free(temp_string);
+		BN_clear(BN_plaintext);
+		BN_clear_free(BN_padding);
 	}
 
 	BN_clear_free(BN_N);
@@ -80,8 +104,9 @@ void rsa_decrypt(string &keyfile, string &inputfile, string &outputfile) {
 
 	//see how many bytes of padding you need to remove
 	//IF SHIT IS BROKEN THIS IS IT
-	padded = (UCHAR)plaintext[plaintext.size() - 1];
-	plaintext.resize(plaintext.size()-padded);
+	cout << padded << endl;
+	//padded = (int)plaintext[plaintext.size() - 1];
+	plaintext.resize(plaintext.size()-padded*2);
 
 	fout.open(outputfile.c_str());
 	fout << plaintext;
