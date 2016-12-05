fb99864cf5420deb2c2d3bc87553f69fe1e36468
Tyler Stuessi <tstuessi@eecs.utk.edu>
2016-12-05 17:43:41 -0500

    Remove these stupid testing files

--- testing/gen_input.py
+++
@@ -1,7 +0,0 @@
-import random
-
-string = ""
-for i in range(random.randint(1, 5000) * 2):
-	string += random.choice(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'])
-print string
-	
--- testing/gen_sym_key.py
+++
@@ -1,7 +0,0 @@
-import random
-
-string = ""
-for i in range(32):
-	string += random.choice(['0', '1', '2', '3', '4', '5', '6', '7', 'a', 'b', 'c', 'd', 'e', 'f'])
-
-print string
